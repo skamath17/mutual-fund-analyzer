6990bf5c95c73d3044cd27cca6469a6b2f1cce16
Sandeep Kamath <kamatsandeep@gmail.com>
2024-11-29 22:48:36 +0530

    Fixed prisma.ts file

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,17 +1,18 @@
 import { PrismaClient } from "@prisma/client";
 
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
 let prisma: PrismaClient;
 
 if (process.env.NODE_ENV === "production") {
   prisma = new PrismaClient();
 } else {
-  // @ts-ignore
-  if (!global.prisma) {
-    // @ts-ignore
-    global.prisma = new PrismaClient();
+  if (!globalForPrisma.prisma) {
+    globalForPrisma.prisma = new PrismaClient();
   }
-  // @ts-ignore
-  prisma = global.prisma;
+  prisma = globalForPrisma.prisma;
 }
 
 export default prisma;
